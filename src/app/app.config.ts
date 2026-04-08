d123af591cc25344de08619fa186f72285a118bd
Lucas Gonçalves <103429796+lucca16bit@users.noreply.github.com>
2026-04-08 01:10:37 +0000

    feat: adicionar PrimeNG e ícones ao projeto, atualizar configurações e estilização

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+	providers: [
+        provideBrowserGlobalErrorListeners(), 
+        provideRouter(routes),
+        providePrimeNG()
+    ],
 };
